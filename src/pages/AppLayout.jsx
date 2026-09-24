bc4ed34ae334a1900f6bc6909cea588a60e0a7dd
Fatemeh Abedniya <fatemehabedniya@gmail.com>
2026-09-24 16:15:17 +0330

    Fix Sidebar import

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,11 +1,11 @@
-import SideBar from "../components/SideBar";
+import Sidebar from "../components/Sidebar";
 import styles from "./AppLayout.module.css";
 import Map from "../components/Map";
 import User from "../components/User";
 export default function AppLayout() {
   return (
     <div className={styles.app}>
-      <SideBar />
+      <Sidebar />
       <Map />
       <User />
     </div>
